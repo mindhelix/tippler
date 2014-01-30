ffbd6018260b6d519d5141d34d725ae4c16495ed
Jinsu Mathew <jinsu@mindhelix.com>
2014-01-30 18:39:48 +0530

    Adds SENDER_ID variable in config

--- config/config.js
+++ config/config.js
@@ -1,8 +1,16 @@
+/*
+ * Tippler config
+ */
+
 module.exports = {
+  // Node server port
   PORT: 3000,
   
-  API_BASE_URL: 'http://localhost/sentinel-api/',
+  // From mail addresss. If using AWS SES, the email id should be the one verified and added in AWS SES console
+  // Can be in this format for better sender info:- SENDER_ID: '"John Doe" <john.doe@tippler.com>'
+  SENDER_ID: 'Put your SES verified email id here',
   
-  AWS_ACCESS_KEY: 'AKIAJYZCRKQOMTUOA6HQ',
-  AWS_SECRET_KEY: '0/dJsLbGiYNSQEKDpM7Um84TF7oydthvePBaTSRc'
+  // AWS keys
+  AWS_ACCESS_KEY: 'Your AWS Access Id key',
+  AWS_SECRET_KEY: 'Your AwS Secret key'
 }
